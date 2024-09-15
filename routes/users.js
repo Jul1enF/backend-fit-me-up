4e75a4dcdc8323c544654be9f22dd223e99ee2a2
Jul1enF <j.furic@gmail.com>
2024-09-15 15:21:29 +0200

    test 3 déploiement

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,17 @@ const User = require('../models/users')
 
 /* GET users listing. */
 router.post('/', async(req, res) => {
-  const {firstname} = req.body
+ try{ const {firstname} = req.body
   const newUser = new User({
     firstname,
   })
 
   await newUser.save()
 
-  res.json({result : true})
+  res.json({result : true})}
+  catch (error){
+    res.json({error})
+  }
 });
 
 module.exports = router;

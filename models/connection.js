311d275d85ea3327d9fe253d547e046f77234b35
Jul1enF <j.furic@gmail.com>
2024-11-01 15:54:02 +0100

    rallongement du timeout connection db

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const connectionString = process.env.CONNECTION_STRING
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+mongoose.connect(connectionString, { connectTimeoutMS: 6000 })
 .then(() => console.log('Database connected'))
 .catch(error => console.error(error));

e4d62b49b34a3f6ef52511c576a561016966772e
Jul1enF <j.furic@gmail.com>
2024-11-03 10:44:58 +0100

    ajout de la connection dans le fichier des routes users

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,21 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/users')
 
+const mongoose = require('mongoose')
+const connectionString = process.env.CONNECTION_STRING
+
+const connect = async ()=> {
+    try {
+        await mongoose.connect(connectionString, { connectTimeoutMS: 6000 })
+         console.log('Database connected')
+     }catch (error){
+         console.log(error)
+     }
+
+}
+
+connect()
+
 const bcrypt = require('bcrypt')
 const uid2 = require('uid2')
 const jwt = require('jsonwebtoken')

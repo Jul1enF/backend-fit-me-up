432a7940d3fe6bc530c027d61225a91fcdb18545
Jul1enF <j.furic@gmail.com>
2024-11-03 10:35:35 +0100

    connection mis en async await

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 const connectionString = process.env.CONNECTION_STRING
 
-mongoose.connect(connectionString, { connectTimeoutMS: 6000 })
-.then(() => console.log('Database connected'))
-.catch(error => console.error(error));
+const connect = async ()=> {
+    try {
+        await mongoose.connect(connectionString, { connectTimeoutMS: 6000 })
+         console.log('Database connected')
+     }catch (error){
+         console.log(error)
+     }
+
+}
+
+connect()

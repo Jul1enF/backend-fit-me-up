41d17d6b3631301c31da51ca64fa88b1719f9ef5
Jul1enF <j.furic@gmail.com>
2024-11-20 15:03:30 +0100

    usefcmv1 false

--- routes/notifications.js
+++ routes/notifications.js
@@ -22,7 +22,7 @@ const sendNotification = async (title, message) => {
 
   let expo = new Expo({
     accessToken: process.env.EXPO_ACCESS_TOKEN,
-  
+    useFcmV1 : false,
     maxConcurrentRequests : 10,
   });
 
